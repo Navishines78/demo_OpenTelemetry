889e4b4697e315da3b3c5db7de3287e1699dc061
Naveen <lyadellanaveen@gmail.com>
2025-04-01 00:41:10 +0530

    roughly configured terraform script

--- terraform/WIP.tf
+++ terraform/WIP.tf
@@ -12,3 +12,17 @@ resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
         user = aws_iam_user.admin1.name
         policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+####################################################################
+
+resource "aws_key_pair" "ssh_key" {
+	name = "key for ssh"
+	public_key = file("~/.ssh/id_rsa.pub")
+}
+####################################################################
+resource "aws_ec2_insatnce" "demo_server" {
+	ami = "ami-sampleeeeeeeeeeeeeeeee"
+	instance_type = "t4.medium"
+	key_name = aws_key_pair.ssh_key.name
+	associate_public_ip_address = true
+}
